61ea063113a9754d855a74a6f363235f2e4636cb
nox491 <57879123+nox491@users.noreply.github.com>
2021-01-07 01:15:12 +0200

    Add files via upload

--- Back-End/app.js
+++ Back-End/app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
 // create express app
 const app = express()
 
-app.use(cors({origin: 'http://localhost:3000'})); 
+app.use(bodyParser.json()) // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
+app.use(cors({origin: 'http://localhost:3000'}));
 
+require('./routes/LoginRoute')(app)
+require('./routes/LogoutRoute')(app)
+require('./routes/AdminAddAlterUserRoute')(app)
+require('./routes/ResetRoute')(app)
+require('./routes/HealthCheckRoute')(app)
 require('./routes/SessionsPerPointRoute')(app)
 require('./routes/SessionsPerStationRoute')(app)
 require('./routes/SessionsPerEVRoute')(app)
