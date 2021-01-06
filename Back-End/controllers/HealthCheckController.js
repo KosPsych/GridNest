a265621f93afc4f321fd456ef07fda5cc9dd38b9
nox491 <57879123+nox491@users.noreply.github.com>
2021-01-07 01:14:41 +0200

    Add files via upload

---
+++ Back-End/controllers/HealthCheckController.js
@@ -0,0 +1,13 @@
+const db = require('../db')
+
+function HealthCheck(req, res) {
+
+	if(db.state === 'disconnected'){
+		res.json({ "status": "failed" })
+	}
+	else {
+		res.json({ "status": "OK" })
+	}
+}
+
+module.exports = HealthCheck
