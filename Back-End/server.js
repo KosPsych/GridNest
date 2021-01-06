97b62f8009b4a90ede14703a25b2a0088e62d11b
nox491 <57879123+nox491@users.noreply.github.com>
2021-01-07 01:48:26 +0200

    Add files via upload

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,9 +1,24 @@
 const app = require('./app')
+const https = require('https');
+const fs = require('fs');
+
+const credentials = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
 
 //connect to the database
 require('./db')
 
+const port = 8765;
+
+app.get('/', (req, res) => {
+   res.send('Now using https..');
+});
+
+var server = https.createServer(credentials, app);
+
 // listen for requests
-app.listen(8765, () => {
-    console.log("Server is listening on port 8765.")
-})
+server.listen(port, () => {
+  console.log("server starting on port : " + port)
+});
