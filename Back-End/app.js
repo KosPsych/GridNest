272555058e1da228a4b2bb966575981fd7094739
KosPsych <kwstaspsychogios13@hotmail.com>
2021-01-06 17:28:47 +0200

    added cors for front-end origin

--- Back-End/app.js
+++ Back-End/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const cors = require('cors')
 // create express app
 const app = express()
 
+app.use(cors({origin: 'http://localhost:3000'})); 
 
 require('./routes/SessionsPerPointRoute')(app)
 require('./routes/SessionsPerStationRoute')(app)
