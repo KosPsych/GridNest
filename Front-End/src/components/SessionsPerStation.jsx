230ab2c00396b484f4e2db068a4f7ab9b7d01de9
KosPsych <kwstaspsychogios13@hotmail.com>
2021-01-07 14:24:15 +0200

    Added SessionsPerStation endpoint

--- Front-End/src/components/SessionsPerStation.jsx
+++ Front-End/src/components/SessionsPerStation.jsx
@@ -1,16 +1,80 @@
 import React , { Component }from 'react';
-import Form from "./Form";
+import axios from 'axios';
+import Table from 'react-bootstrap/Table'
+  class SessionsPerStation extends Component {
+      state = { 
+         Sessions :[] , 
+         second_render :false
+       }
+       
 
-class SessionsPerStation extends Component {
-    state = {  }
-    render() { 
-        return ( 
-         <div>
-             <Form />
-         </div>
+       async componentDidMount(){
+           
+        const {data: Sessions}= await axios.get("http://localhost:8765/evcharge/api/SessionsPerStation/"+this.props.match.params.stationID+"/"+this.props.match.params.datefrom+"/"+this.props.match.params.dateto);
+        this.state.second_render=true
+        this.setState({Sessions})
+    
+       }
 
-         );
-    }
-}
- 
-export default SessionsPerStation;
+      
+      render() { 
+
+          if(this.state.second_render){  
+          
+          
+          return ( 
+            
+            
+            
+              
+            <React.Fragment>
+              <ul>
+               
+               <li>StationID : {this.props.match.params.stationID}</li>
+              
+               <li>Operator : {this.state.Sessions.Operator}</li>  
+               <li>RequestTimestamp : {this.state.Sessions.RequestTimestamp }</li>
+               <li>PeriodFrom : {this.state.Sessions.PeriodFrom}</li>
+               
+               <li>PeriodTo : {this.state.Sessions.PeriodTo}</li>
+              
+               <li>TotalEnergyDelivered : {this.state.Sessions.TotalEnergyDelivered}</li>
+               <li>NumberOfActivePoints : {this.state.Sessions.NumberOfActivePoints}</li>
+               
+               <li>NumberOfChargingSessions : {this.state.Sessions.NumberOfChargingSessions}</li>
+               
+               
+             </ul>
+
+               <Table striped bordered hover>
+                 <thead>
+                   <tr> 
+                     <th>PointID</th>
+                     <th>PointSessions</th>
+                     <th>EnergyDelivered</th>
+                   </tr>
+                 </thead>
+  <tbody>
+  {this.state.Sessions.SessionsSummaryList.map(Session => (
+  <tr key={Session.PointID}>
+    <th>{Session.PointID}</th>
+    <th>{Session.PointSessions}</th>
+    <th>{Session.EnergyDelivered}</th>
+    </tr>  
+      ))}
+  </tbody>
+</Table>
+  </React.Fragment>        
+            
+            
+          
+          );
+
+  }
+  else{
+    return <h1>Waiting Server...</h1>
+  }
+      }
+  }
+   
+  export default SessionsPerStation;
