ecabdfa1f2855d7a9d90908f24e925b470369be0
nick Zervas <nickzpower2@gmail.com>
2021-01-09 00:38:59 +0200

    Delete AdminAddAlterUserRoute.js

--- Back-End/controllers/AdminAddAlterUserRoute.js
+++
@@ -1,6 +0,0 @@
-module.exports = (app) => {
-    const Add_Alter_User = require('../controllers/AdminAddAlterUserController')
-    const verifyToken = require('../controllers/VerifyTokenController')
-
-app.post('/evcharge/api/admin/usermod/:username/:password', verifyToken, Add_Alter_User)
-}
