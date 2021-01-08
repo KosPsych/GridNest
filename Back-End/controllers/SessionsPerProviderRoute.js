0314ac440441394ad75bd72765f274fe14e5a4ee
nick Zervas <nickzpower2@gmail.com>
2021-01-09 00:39:54 +0200

    Delete SessionsPerProviderRoute.js

--- Back-End/controllers/SessionsPerProviderRoute.js
+++
@@ -1,6 +0,0 @@
-module.exports = (app) => {
-    const getSessionsPerProvider = require('../controllers/SessionsPerProviderController')
-    const verifyToken = require('../controllers/VerifyTokenController')
-    
-app.get('/evcharge/api/SessionsPerProvider/:providerID/:yyyymmdd_from/:yyyymmdd_til',verifyToken,getSessionsPerProvider)
-}
