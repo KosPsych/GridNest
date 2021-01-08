26f663ef094db470bd60caa1f47f59877e049d66
nick Zervas <nickzpower2@gmail.com>
2021-01-09 00:40:06 +0200

    Delete SessionsPerStationRoute.js

--- Back-End/controllers/SessionsPerStationRoute.js
+++
@@ -1,6 +0,0 @@
-module.exports = (app) => {
-    const getSessionsPerStation = require('../controllers/SessionsPerStationController')
-    const verifyToken = require('../controllers/VerifyTokenController')
-
-app.get('/evcharge/api/SessionsPerStation/:stationID/:yyyymmdd_from/:yyyymmdd_til',verifyToken,getSessionsPerStation)
-}
