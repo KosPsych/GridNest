95a8cf06570c79781c76cd47fdd41ddda0525ac9
nick Zervas <nickzpower2@gmail.com>
2021-01-09 00:39:44 +0200

    Delete SessionsPerPointRoute.js

--- Back-End/controllers/SessionsPerPointRoute.js
+++
@@ -1,6 +0,0 @@
-module.exports = (app) => {
-    const getSessionsPerPoint = require('../controllers/SessionsPerPointController')
-    const verifyToken = require('../controllers/VerifyTokenController')
-
-app.get('/evcharge/api/SessionsPerPoint/:pointID/:yyyymmdd_from/:yyyymmdd_til',verifyToken,getSessionsPerPoint)
-}
