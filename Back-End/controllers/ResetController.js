486b17af1ff29244635bf40cdb9742d7ffc63603
nox491 <57879123+nox491@users.noreply.github.com>
2021-01-09 16:20:37 +0200

    Add files via upload

--- Back-End/controllers/ResetController.js
+++ Back-End/controllers/ResetController.js
@@ -7,10 +7,11 @@ function resetsessions(req, res) {
   var isAdmin = '2'
   password = bcrypt.hashSync(password, 10);
 
-  let check_def_admin_existence =  "SELECT * FROM Users WHERE isAdmin='2'";
+  let check_def_admin_existence =  "SELECT * FROM Users WHERE Username='admin'";
   db.query(check_def_admin_existence, (error,  result1) => {
     if(error) throw 'query failed';
     else if(result1=='') {
+      console.log('Admin not in database. Adding...')
       let reset_query = 'DELETE FROM Sessions; INSERT INTO users (Username, Password, isAdmin) VALUES (?, ?, ?)';
       db.query(reset_query, [username, password, isAdmin],(err, result) => {
         if(err) res.json({ "status": "failed" })
@@ -18,8 +19,9 @@ function resetsessions(req, res) {
       })
     }
     else  {
-      reset_def_admin = 'UPDATE Users SET Username = ?, Password = ? WHERE isAdmin = ?'
-      db.query(reset_def_admin, [username,password, isAdmin], (error1, result2)  =>  {
+      console.log('Admin already in database. Changing password')
+      let reset_def_admin = 'DELETE FROM Sessions; UPDATE Users SET Password = ? WHERE Username=?';
+      db.query(reset_def_admin, [password, username], (error1, result2)  =>  {
         if(error1) res.json({ "status": "failed" })
         else res.json({ "status": "OK" })
       })
