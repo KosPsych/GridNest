aeb55669b53e72aba113f15580c4a915c0584dd6
KosPsych <kwstaspsychogios13@hotmail.com>
2021-01-12 15:23:27 +0200

    fixed

--- Back-End/controllers/AdminAddSessionsController.js
+++ Back-End/controllers/AdminAddSessionsController.js
@@ -20,10 +20,10 @@ function AddSessions(req,res){
             let insert_query = "INSERT IGNORE  INTO Sessions(SessionID, StationID,PointID, Username, VehicleID, connectionTime,"+
             " doneCharginTime, Timezone ,KwhDelivered ,Protocol,Payment )  VALUES ?";
             db.query(insert_query, [csvData], (err, db_res) => {
-                if (err) console.log(err)
+                if (err) res.send("Incorrect Data format for  Sessions or File is too big(larger than 1GB")
                 let count_query = "SELECT Count(*) as count FROM Sessions";
                 db.query(count_query, (err1, result) => {
-                    if (err1) console.log(err)
+                    if (err1) res.send(err1.message)
                     else {
                         res.send({SessionsInUploadedFile :db_res.affectedRows +  db_res.warningCount , SessionsImported : db_res.affectedRows, TotalSessionsInDatabase :result[0].count});
 
--- Back-End/controllers/VerifyTokenController.js
+++ Back-End/controllers/VerifyTokenController.js
@@ -4,14 +4,10 @@ var fs = require('fs');
 var secretToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE2MDk5Njk2NjEsImV4cCI6MTY0MTUwNTY2MSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.X0AKkr4AXvRRgg0C9_yjvhOV7najxgQN4xbS57Y7GGQ"
 
 function verifyToken(req, res, next) {
-<<<<<<< HEAD
+
 
     const token  = req.headers['x-observatory-auth']
-=======
-    
-    const token  = req.headers['x-observatory-auth'];
-    
->>>>>>> FrontAdmin
+
     if (!token)  res.status(401).send('Not Authorized')
     else{
         var dir = './Tokens'
