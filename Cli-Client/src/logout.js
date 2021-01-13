058c8ae1d1151829f5214a18fc3d2a99e8be4512
nick Zervas <nickzpower2@gmail.com>
2021-01-13 18:43:04 +0200

    Add logout file

---
+++ Cli-Client/src/logout.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+const chalk = require('chalk');
+const checkParams = require('./utils/checkParams');
+const createURL = require('./utils/createURL');
+
+module.exports = function(object){
+
+    
+    //Check for parameters
+    
+    if( checkParams('Logout', object.apikey) ){
+        console.log(chalk.red('Error! Please insert your unique apikey to Logout'));
+    }
+
+    //if(1==0) {}
+    else{
+
+        //Create url
+        let baseUrl = createURL('/Logout');
+        //const baseUrl = 'http://localhost:8765/evcharge/api/logout'
+
+        // add apikey to headers 
+        const config = {
+            headers: {
+                'X-OBSERVATORY-AUTH' : object.apikey
+            }
+        }
+
+                    
+        //Send get request
+
+        axios.post(baseUrl, {},config )
+        .then(res => {
+                console.log(chalk.green('User successfully logged out'));
+            })
+        .catch(err => {
+            console.log(chalk.red(err.message));
+            console.log(chalk.red(err.response.data));
+        })
+        return;
+    }
+
+};
