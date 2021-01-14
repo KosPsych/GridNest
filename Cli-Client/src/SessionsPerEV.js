73b88e6ec10e249b69ba9c465584edeee647430b
nick Zervas <nickzpower2@gmail.com>
2021-01-14 17:00:46 +0200

    add 4 basic endpoint request files  in cli

---
+++ Cli-Client/src/SessionsPerEV.js
@@ -0,0 +1,52 @@
+const axios = require('axios');
+const chalk = require('chalk');
+const checkParams = require('./utils/checkParams');
+const createURL = require('./utils/createURL');
+
+module.exports = function(object){
+
+    
+    //Check for parameters
+    
+    if( checkParams('SessionsPerStations', object.apikey) ){
+        console.log(chalk.red('Error! Please enter correct parameters:'));
+        console.log(chalk.green('Mandatory Parameters:'));
+        console.log(chalk.green('--ev                  ex: 5'));
+        console.log(chalk.green('--datefrom            ex: 20181009'));
+        console.log(chalk.green('--dateto              ex: 20181016'));
+        console.log(chalk.green('--format              ex: csv'));
+        console.log(chalk.green('--apikey              ex: asdasdmadmadxmawmdxamdma'));
+    }
+
+    //if(1==0) {}
+    else{
+
+        //Create url
+        let baseUrl = createURL('/SessionsPerStation',object.ev,object.datefrom,object.dateto,object.format,object.token);
+        //const baseUrl = 'https://localhost:8765/evcharge/api/SessionsPerEV/5/20181009/20181016?format=csv'
+
+        // add apikey to headers 
+        const config = {
+            headers: {
+                'x-observatory-auth' : object.apikey
+            }
+        }
+
+                    
+        //Send get request
+
+        axios.get(baseUrl,config )
+        .then(res => {
+            if (object.format =='csv')
+            console.log(chalk.green(res.data));
+            else 
+            console.log(res.data);
+            })
+        .catch(err => {
+            console.log(chalk.red(err.message));
+            console.log(chalk.red(err.response.data));
+        })
+        return;
+    }
+
+};
---
+++ Cli-Client/src/SessionsPerPoint.js
@@ -0,0 +1,52 @@
+const axios = require('axios');
+const chalk = require('chalk');
+const checkParams = require('./utils/checkParams');
+const createURL = require('./utils/createURL');
+
+module.exports = function(object){
+
+    
+    //Check for parameters
+    
+    if( checkParams('SessionsPerStations', object.apikey) ){
+        console.log(chalk.red('Error! Please enter correct parameters:'));
+        console.log(chalk.green('Mandatory Parameters:'));
+        console.log(chalk.green('--point               ex: 1-1-178-817'));
+        console.log(chalk.green('--datefrom            ex: 20181009'));
+        console.log(chalk.green('--dateto              ex: 20181016'));
+        console.log(chalk.green('--format              ex: csv'));
+        console.log(chalk.green('--apikey              ex: asdasdmadmadxmawmdxamdma'));
+    }
+    
+    //if(1==0) {}
+    else{
+
+        //Create url
+        let baseUrl = createURL('/SessionsPerStation',object.point,object.datefrom,object.dateto,object.format,object.token);
+        //const baseUrl = 'https://localhost:8765/evcharge/api/SessionsPerPoint/1-1-178-817/20181009/20181016?format=csv'
+
+        // add apikey to headers 
+        const config = {
+            headers: {
+                'x-observatory-auth' : object.apikey
+            }
+        }
+
+                    
+        //Send get request
+
+        axios.get(baseUrl,config )
+        .then(res => {
+            if (object.format =='csv')
+            console.log(chalk.green(res.data));
+            else 
+            console.log(res.data);
+            })
+        .catch(err => {
+            console.log(chalk.red(err.message));
+            console.log(chalk.red(err.response.data));
+        })
+        return;
+    }
+
+};
