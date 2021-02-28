e0ec37dc0eb830e8145e00acf04756a015349fa8
nox491 <57879123+nox491@users.noreply.github.com>
2021-02-28 03:50:08 +0200

    Add files via upload

--- Back-End/controllers/AdminAddSessionsController.js
+++ Back-End/controllers/AdminAddSessionsController.js
@@ -3,29 +3,30 @@ const fastcsv = require("fast-csv");
 const db = require('../db');
 
 function AddSessions(req,res){
+    if(req.user.isAdmin!='1' && req.user.isAdmin!='2') res.status(401).send('Lack of Authorization')
+    else {
+      let stream = fs.createReadStream("./upload_folder/upload_file.csv");
+      let csvData = [];
 
-    let stream = fs.createReadStream("./upload_folder/upload_file.csv");
-    let csvData = [];
-    
-    let csvStream = fastcsv
-        .parse()
-        .on("data", function(data) {
-        csvData.push(data);
-        })
-        .on("end", function() {
-            // remove the first line: header
-            csvData.shift();
-    
-            let insert_query = "INSERT IGNORE  INTO Sessions(SessionID, StationID,PointID, Username, VehicleID, connectionTime,"+
-            " doneCharginTime, Timezone ,KwhDelivered ,Protocol,Payment )  VALUES ?";
-            db.query(insert_query, [csvData], (err, db_res) => {
-                if (err) res.send("Incorrect Data format for  Sessions or File is too big(larger than 1GB)")
-                else{
-                    let count_query = "SELECT Count(*) as count FROM Sessions";
-                    db.query(count_query, (err1, result) => {
-                        if (err1) res.send(err1.message)
-                        else {
-                            res.send({SessionsInUploadedFile :db_res.affectedRows +  db_res.warningCount , SessionsImported : db_res.affectedRows, TotalSessionsInDatabase :result[0].count});
+      let csvStream = fastcsv
+          .parse()
+          .on("data", function(data) {
+            csvData.push(data);
+          })
+          .on("end", function() {
+              // remove the first line: header
+              csvData.shift();
+
+              let insert_query = "INSERT IGNORE  INTO Sessions(SessionID, StationID,PointID, Username, VehicleID, connectionTime,"+
+              " doneCharginTime, Timezone ,KwhDelivered ,Protocol,Payment )  VALUES ?";
+              db.query(insert_query, [csvData], (err, db_res) => {
+                  if (err) res.send("Incorrect Data format for  Sessions or File is too big(larger than 1GB)")
+                  else{
+                      let count_query = "SELECT Count(*) as count FROM Sessions";
+                      db.query(count_query, (err1, result) => {
+                          if (err1) res.send(err1.message)
+                          else {
+                              res.send({SessionsInUploadedFile :db_res.affectedRows +  db_res.warningCount , SessionsImported : db_res.affectedRows, TotalSessionsInDatabase :result[0].count});
 
 
                         }
@@ -34,8 +35,9 @@ function AddSessions(req,res){
             });
 
         }
-        );
 
+        );
     stream.pipe(csvStream);
+  }
 }
 module.exports = AddSessions
